aadfcdc592ba5c4129061435bc78a322e427642b
Anna Zimenkina <anuta199212@yandex.ru>
2018-05-14 22:15:33 +0700

    comments added

--- filemanager.cpp
+++ filemanager.cpp
@@ -21,31 +21,30 @@
 #define box_on	1
 #define box_off	0
 
-WINDOW *create_new_window(int x, int y, int w, int h, int color, int box_state);
-char** directory_read2(WINDOW *win, unsigned *items_counter, char** input_items, int c);
-char * items_array[] = {"Left panel", "Rigth_panel",  (char *)NULL };
+WINDOW *create_new_window(int startx, int starty, int width, int height, int color, int box_state);
+char** directory_read(WINDOW *win, unsigned *items_counter, char** input_items, int c);
+char * items_array[] = {"Left panel", "Rigth panel",  (char *)NULL };
 ITEM ** my_items;
 //---------------------------------------------------------------- 
 int main (void)
 {
-	WINDOW *window_rigth;
-	WINDOW *window_left;
-	WINDOW *current_window;
-	WINDOW *key_window;
-	MENU *my_menu;
-	WINDOW *menu_win;
-	int n_menuitems;
-	int key = 0;
-	int position_left = 0;
-	int position_right = 0;
-	int menu_key = 0;
-
-	char** items_left;
+	WINDOW *window_rigth;		//right panel
+	WINDOW *window_left;		//left panel
+	WINDOW *current_window;		
+	WINDOW *key_window;		//window with a description of the keys
+	MENU *my_menu;			//menu for selecting current panel
+	WINDOW *menu_win;		//menu window
+	int n_menuitems;		//number of menu items
+	int key = 0;			//pressed key
+	int position_left = 0;		//the current cursor position in the left panel
+	int position_right = 0;		//the current cursor position in the right panel
+	int menu_key = 0;		//pressed key in menu window
+	char** items_left;		//array of directories in the left panel
 	unsigned items_counter_left = 0;
-
-	char** items_rigth;
+	char** items_rigth;		//array of directories in the right panel
 	unsigned items_counter_rigth = 0;
 
+	/*screen initialisation*/
 	initscr();	
 	start_color();
 	init_pair(WHITE, COLOR_WHITE, COLOR_BLUE);
@@ -66,6 +65,7 @@ int main (void)
 	wbkgd(stdscr, COLOR_PAIR(WHITE));
 	refresh();
 
+	/*creating a menu for selecting the panel*/
 	n_menuitems = (sizeof(items_array)/sizeof(items_array[0]));
 	my_items = (ITEM **) calloc(n_menuitems +1, sizeof(ITEM *));
 	for(int i = 0; i < n_menuitems; i++)
@@ -79,29 +79,30 @@ int main (void)
 	wattron(menu_win, COLOR_PAIR(MENU_COLOR));
 	mvwprintw(menu_win, 0, 2, "Panel");
 	wattroff(menu_win, COLOR_PAIR(MENU_COLOR));
-
 	refresh();
 	wrefresh(menu_win);
-	
+
+	/*create a window with a description of the keys */
 	key_window = create_new_window(0, LINES-1, COLS, 1, MENU_COLOR, box_off);
 	window_left = create_new_window(0, 1, COLS/2-1, LINES-2, WHITE, box_on);
 	window_rigth = create_new_window(COLS/2, 1, COLS/2-1, LINES-2, WHITE, box_on);
 	scrollok(window_left, TRUE);
 	scrollok(window_rigth, TRUE);
 	current_window = window_left;
+	
+	/*read the current directory and print the list */ 
+	items_left = directory_read(window_left, &items_counter_left, items_left, 0);
+	items_rigth = directory_read(window_rigth, &items_counter_rigth, items_rigth, 0);
 
-	items_left = directory_read2(window_left, &items_counter_left, items_left, 0);
-	items_rigth = directory_read2(window_rigth, &items_counter_rigth, items_rigth, 0);
-
-	mvwprintw(key_window, 0, 0, "F3 - open directory	F9 - Panel	F10 - Quit");
+	mvwprintw(key_window, 0, 0, "F3 - open directory\t9 - Panel\tF10 - Quit");
 	wrefresh(key_window);
 	wrefresh(window_left);
 	wrefresh(window_rigth);
 
 	while(1)
-	{
+	{	/*reading the pressed key*/
 		key = wgetch(current_window);
-
+		/*highlighting the first line of the list*/
 		wattroff(current_window, A_STANDOUT);
 		if (current_window == window_left)
 		{
@@ -112,9 +113,10 @@ int main (void)
 			mvwprintw(current_window, position_right+1,2,"%s", *(items_rigth+(position_right)));
 		}
 		wrefresh(current_window);
+		/*processing of the pressed key*/
 		switch(key)
 		{
-			case KEY_UP:
+			case KEY_UP:	//move up one item in the directory list
 				if (current_window == window_left)
 				{
 					position_left--;
@@ -128,7 +130,7 @@ int main (void)
 						position_right = 0;
 				}
 				break;
-			case KEY_DOWN:
+			case KEY_DOWN:	//move down one item in the directory list
 				if (current_window == window_left)
 				{
 					position_left++;
@@ -142,25 +144,23 @@ int main (void)
 						position_right = 0;
 				}
 				break;
-			case KEY_F(3):
+			case KEY_F(3):	//open directory
 				if (current_window == window_left)
 				{
-					items_left = directory_read2(current_window, &items_counter_left, items_left, position_left);
+					items_left = directory_read(current_window, &items_counter_left, items_left, position_left);
 				position_left = 0;
 				}
 				else if (current_window == window_rigth)
 				{
-					items_rigth = directory_read2(current_window, &items_counter_rigth, items_rigth, position_right);
+					items_rigth = directory_read(current_window, &items_counter_rigth, items_rigth, position_right);
 				position_right = 0;
 				}
 				break;
-			case KEY_F(10):
+			case KEY_F(10):	//quit the program
 				unpost_menu(my_menu);
 				free_menu(my_menu);
 				for(int i = 0; i < n_menuitems; i++)
-				{
 					free_item(my_items[i]);
-				}
 				for(int i = 0; i<items_counter_left;i++)
 					free(items_left[i]);
 				free(items_left);
@@ -177,25 +177,25 @@ int main (void)
 				endwin();
 				return(0);
 				break;
-			case KEY_F(9):
+			case KEY_F(9):	//display the menu
 				post_menu(my_menu);
 				wclear(key_window);
-				mvwprintw(key_window, 0, 0, "F3 - Select item	F10 - Quit");
+				mvwprintw(key_window, 0, 0, "F3 - Select item\tF10 - Quit");
 				wrefresh(key_window);
 				while ((menu_key = wgetch(menu_win)) != KEY_F(10))
 				{
 					switch(menu_key)
 					{
-						case KEY_F(3):
+						case KEY_F(3):	//selecting the current panel
 							if (current_item(my_menu) == my_items[0])
 								current_window = window_left;
 							else if (current_item(my_menu) == my_items[1])
 								current_window = window_rigth;
 							break;
-						case KEY_DOWN:
+						case KEY_DOWN:	//move down one item in the menu
 							menu_driver(my_menu, REQ_DOWN_ITEM);
 							break;
-						case KEY_UP:
+						case KEY_UP://move up one item in the menu
 							menu_driver(my_menu, REQ_UP_ITEM);
 							break;
 					}
@@ -208,7 +208,7 @@ int main (void)
 				box(window_rigth, 0, 0);
 				box(window_left, 0, 0);
 
-				mvwprintw(key_window, 0, 0, "F3 - open directory	F9 - Panel	F10 - Quit");
+				mvwprintw(key_window, 0, 0, "F3 - open directory\tF9 - Panel\tF10 - Quit");
 				wrefresh(key_window);
 				refresh();
 				wrefresh(menu_win);
@@ -216,7 +216,8 @@ int main (void)
 				wrefresh(window_left);
 				break;
 		}
-	
+		
+		/*highlighting the current line of the list*/
 		wattron(current_window, A_STANDOUT);
 		
 		if (current_window == window_left)
@@ -233,7 +234,7 @@ int main (void)
 	}
 }
 //------------------------------------------------
-char** directory_read2(WINDOW *win, unsigned *items_counter, char** input_items, int c)
+char** directory_read(WINDOW *win, unsigned *items_counter, char** input_items, int c)
 {
 	DIR *d;
 	struct dirent *dir;
@@ -331,7 +332,7 @@ char** directory_read2(WINDOW *win, unsigned *items_counter, char** input_items,
 				break;
 		}
 		item_length = sizeof(dir->d_name);
-		if ((*items_counter) >= size)
+		if ((*items_counter) >= size) 
 		{
 			size++;
 			input_items = (char**) realloc(input_items, size*sizeof(char*));
@@ -349,11 +350,11 @@ char** directory_read2(WINDOW *win, unsigned *items_counter, char** input_items,
 	return(input_items);
 }
 //---------------------------------------------------------------- 
-WINDOW *create_new_window(int x, int y, int w, int h, int color, int box_state)
+WINDOW *create_new_window(int startx, int starty, int width, int height, int color, int box_state)
 {
 	WINDOW *local_win;
 
-	local_win = newwin(h,w,y,x);
+	local_win = newwin(height,width,starty,startx);
 	if (box_state)
 		box(local_win, 0, 0);
 
